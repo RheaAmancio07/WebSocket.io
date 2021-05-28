bbb9e67e9864748c5340d71674c1efa5c646664b
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2021-05-28 14:49:34 +0800

    this is for websocket

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -16,84 +15,93 @@ app.use(express.static(__dirname + '/public'));
 // Holds an array of users connected
 var nicknames = [];
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
 
-    // When user enters name from the client
-	socket.on('new user', function(data) {
+	// When user enters name from the client
+	socket.on('new user', function (data) {
 
 		var nicknameTaken;
 
-        // Ensure submitted username doesn't already exist
+		// Ensure submitted username doesn't already exist
 		// Case insensitive checking
-		nicknames.forEach(function(name){
-			if ( name.toLowerCase() === data.nickname.toLowerCase() ) {
-                nicknameTaken = true; // Set nicknameTaken to true if name already exists
+		nicknames.forEach(function (name) {
+			if (name.toLowerCase() === data.nickname.toLowerCase()) {
+				nicknameTaken = true; // Set nicknameTaken to true if name already exists
 				return;
 			}
 		});
 
-		if ( nicknameTaken ) {
-            // Send notification to client that username already exists
+		if (nicknameTaken) {
+			// Send notification to client that username already exists
 			socket.emit('nickname taken');
 
 		} else {
-            // Or else create new username
-			socket.set("nickname", data.nickname, function() {
+			// Or else create new username
+			socket.set("nickname", data.nickname, function () {
 
 				// Update 'nicknames' array
 				nicknames.push(data.nickname);
 
-                // Welcome the user who joined
+				// Welcome the user who joined
 				socket.emit('welcome', data.nickname, nicknames);
 
-                // Broadcast to other clients that a user has joined
+				// Broadcast to other clients that a user has joined
 				socket.broadcast.emit('user joined', data.nickname, nicknames);
 			});
 
 		}
-		
+
 	});
+	// // when the client emits 'typing', we broadcast it to others
+	// socket.on("typing", function (data) {
+	// 	socket.broadcast.emit("typing", data);
+	// });
 
-    // Listening for chat messages being sent
-	socket.on('outgoing', function(data) {
+	// socket.on("stop-typing", function (data) {
+	// 	socket.broadcast.emit("stop-typing", data);
+	// });
 
-		socket.get('nickname', function(err,nickname) {
+
+	// Listening for chat messages being sent
+	socket.on('outgoing', function (data) {
+
+		socket.get('nickname', function (err, nickname) {
 			var eventArgs = {
 				nickname: nickname,
 				message: data.message
 			};
 
-            // You can either use the code below
-            // to send messages to all clients...
+			// You can either use the code below
+			// to send messages to all clients...
 			//io.sockets.emit('incoming', nameAndData, null);
 
-            // ...Or you can use these lines for better flexibility
+			// ...Or you can use these lines for better flexibility
 			socket.emit('incoming', eventArgs, true);
 			socket.broadcast.emit('incoming', eventArgs, false);
 		});
 	});
 
-    // Listening for when someone leaves - native listener for socket.io
-	socket.on('disconnect', function(){
+	// Listening for when someone leaves - native listener for socket.io
+	socket.on('disconnect', function () {
 
-		socket.get('nickname', function(err, nickname){
+		socket.get('nickname', function (err, nickname) {
 
 			// Remove username from users
-			nicknames.splice( nicknames.indexOf(nickname), 1 );
+			nicknames.splice(nicknames.indexOf(nickname), 1);
 
-            // Don't need to broadcast if there are no users left
-			if(nicknames.length === 0) return;
+			// Don't need to broadcast if there are no users left
+			if (nicknames.length === 0) return;
 
-            // Notify existing users that someone left
+			// Notify existing users that someone left
 			socket.broadcast.emit('user left', nickname, nicknames);
 		});
-		
+
 		console.log('user disconnected!');
 	});
 });
 
 
-server.listen(port, function() { console.log("Server listening at http://localhost:"+port)});
+server.listen(port, function () { console.log("Server listening at http://localhost:" + port) });
 
 // ExpressJS routes using the 'get' verb
 app.get('/', handler);
